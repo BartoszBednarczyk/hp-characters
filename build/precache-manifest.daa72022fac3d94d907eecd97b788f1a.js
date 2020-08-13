b8f9300bd2741ffcb630f7b53d207a0c8e71f3e9
Bartosz Bednarczyk <bfbednarczyk@gmail.com>
2020-08-13 13:07:37 +0200

    v 1.0 with corrected .gitignore

---
+++ build/precache-manifest.daa72022fac3d94d907eecd97b788f1a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b52ec59a5dd4427ec30cd66d75d1a9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4e69bb7c6be228c704c",
+    "url": "/static/css/main.07badea8.chunk.css"
+  },
+  {
+    "revision": "76a7c64da9f8390f32b3",
+    "url": "/static/js/2.2a026927.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.2a026927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4e69bb7c6be228c704c",
+    "url": "/static/js/main.f7f2cf4f.chunk.js"
+  },
+  {
+    "revision": "072c0ed864e78dc72a97",
+    "url": "/static/js/runtime-main.95dc5144.js"
+  },
+  {
+    "revision": "181ef9a7aee45c119e68f294e8426a3a",
+    "url": "/static/media/HARRYP.181ef9a7.TTF"
+  },
+  {
+    "revision": "5dba1eb623400246c0279d45032939be",
+    "url": "/static/media/Ministry_Logo_BLACK.5dba1eb6.svg"
+  },
+  {
+    "revision": "a8f55883aaaa6232d8a7729ccba9f5c9",
+    "url": "/static/media/deathEater.a8f55883.svg"
+  },
+  {
+    "revision": "821f9e291f9fc77eca1942c03f807dab",
+    "url": "/static/media/dumbledoresArmy.821f9e29.svg"
+  },
+  {
+    "revision": "4608b57490fde66702d38b363ed9d551",
+    "url": "/static/media/logo.4608b574.png"
+  },
+  {
+    "revision": "51852a31bf6c042bb3e374d5450d172e",
+    "url": "/static/media/orderOfThePhoenix.51852a31.svg"
+  }
+]);
