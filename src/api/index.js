5c9fe874f03001d1900e02379d187dd299f8d50f
Bartosz <bfbednarczyk@gmail.com>
2021-03-13 15:02:46 +0100

    Fixed API connection

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
-const url = "https://www.potterapi.com/v1/"
+const url = "https://raw.githubusercontent.com/JRC404/HarryPotterAPI/master/JsonFiles/HarryPotter.json"
 
 const key = "?key=$2a$10$nxifFQ9eGSnhYGxluBj1L.83HGaW2jUI85qCpf9OBza9ftyhpBDQO"
 
 export const fetchData = async () => {
     try {
-        const { data } = await axios.get(`${url}characters${key}`)
+        const { data } = await axios.get(`${url}`)
         console.log(data)
         return await data
         
