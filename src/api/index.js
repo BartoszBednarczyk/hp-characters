9a6184090cd60f083a91bff3f226c3d3b32546fe
Bartosz <bfbednarczyk@gmail.com>
2021-03-13 15:13:40 +0100

    CORS testing

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = "https://raw.githubusercontent.com/JRC404/HarryPotterAPI/master/JsonFiles/HarryPotter.json"
+const url = "https://raw.githubusercontent.com/BartoszBednarczyk/hp-characters/master/src/api/hp-characters.json"
 
 const key = "?key=$2a$10$nxifFQ9eGSnhYGxluBj1L.83HGaW2jUI85qCpf9OBza9ftyhpBDQO"
 
